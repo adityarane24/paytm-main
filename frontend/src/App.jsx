c093241e10ce1c0a98362dfdb03ed4d5c004a5d1
Aditya <adityarane822@gmail.com>
2026-06-15 22:42:20 +0530

    Fix casing mismatch across all page file components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/Signin";
-import { Dashboard } from "./pages/Dashboard";
-import { SendMoney } from "./pages/SendMoney";
+import { Signup } from "./pages/signup";
+import { Signin } from "./pages/signin";
+import { Dashboard } from "./pages/dashboard";
+import { SendMoney } from "./pages/sendmoney";
 
 function App() {
   return (
